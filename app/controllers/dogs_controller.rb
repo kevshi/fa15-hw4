006f47cbb7a8a0da7374f290bca73f9c31e2adaf
Kevin Shi <kshi@berkeley.edu>
2015-10-09 22:40:16 -0700

    finally done

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,29 @@
+class DogsController < Application Controller
+	def index
+		@dogs = Dog.all
+	end
+
+	def new
+		@dogs = Dog.new
+	end
+
+	def create
+		@dog = Dog.create(dog_params)
+		if @dog.save
+			redirect_to @dog
+
+		else
+			render "new"
+		end
+	end
+
+	def show
+		@dog = Dog.find(params[:id])
+	end
+
+	private
+	
+	def dog_params
+		params.require(:dog).permit(:name, :age)
+	end
+end
